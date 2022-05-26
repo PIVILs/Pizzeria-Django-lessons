1c68c05bf7a5ce9cc71642be0b2c45a1a0f6da6e
PIVILs <PIVILs@mail.ru>
2022-05-26 17:21:30 +0400

    add info pages for each pizza

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -8,4 +8,10 @@ app_name = 'pizzas'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+
+    # Вывод всех товаров.
+    path('pizzas/', views.pizzas, name='pizzas' ),
+
+    # Страница с подробной информацией по товару
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Pizza
 
 def index(request):
     """Домашняя страница приложения Pizzeria"""
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    """Выводит список пицц."""
+    pizzas = Pizza.objects.all()
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    """Выводит одну пиццу и всю по ней информацию."""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizzas/pizza.html', context)
